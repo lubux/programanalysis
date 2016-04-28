6e966ff5e09a9c870847a9171d1d8b232e86bd79
Cedric Baumann <Cedric Baumann>
2016-04-28 10:18:27 +0200

    try to implement the nodetransfer

--- ProgramAnalysis/src/com/programanalysis/lattice/State.java
+++ ProgramAnalysis/src/com/programanalysis/lattice/State.java
@@ -3,17 +3,12 @@ package com.programanalysis.lattice;
 import com.programanalysis.monitoring.iMonitoring;
 import com.programanalysis.util.Tuple;
 import dk.brics.tajs.flowgraph.BasicBlock;
-import dk.brics.tajs.monitoring.IAnalysisMonitoring;
+import dk.brics.tajs.flowgraph.Function;
 import dk.brics.tajs.solver.GenericSolver;
-import dk.brics.tajs.solver.ICallEdge;
-import dk.brics.tajs.solver.IContext;
 import dk.brics.tajs.solver.IState;
-import com.programanalysis.lattice.Context;
+import com.programanalysis.util.VariableName;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * Created by cedri on 4/24/2016.
@@ -21,48 +16,101 @@ import java.util.Set;
 public class State implements IState <State, Context, CallEdge> {
 
 
-    /** store from global variable name to abstract object*/
-    private Map<String, Set<AbstractObject>> globalstore;
-
-    /** store from local variable name to abstract object*/
-    private Map<String, Set<AbstractObject>> localstore;
+    /** store from variable name to abstract object*/
+    private static Map<String, Set<AbstractObject>> store = new HashMap<String, Set<AbstractObject>>();;
 
     /** store from abstract object and field to abstract object*/
-    private Map<Tuple<AbstractObject, String>, Set<AbstractObject>> fieldstore;
+    private static Map<Tuple<AbstractObject, String>, Set<AbstractObject>> propertystore = new HashMap<Tuple<AbstractObject, String>, Set<AbstractObject>>();;
 
     private BasicBlock block;
 
     private Context context;
 
-    /** used for the node visitor of newobjectnode*/
-    public AbstractObject newObject;
+    private List<Object> registers;
+
+    private GenericSolver<State, Context, CallEdge, iMonitoring, ?>.SolverInterface solver;
 
 
     public State(GenericSolver<State, Context, CallEdge, iMonitoring, ?>.SolverInterface c, BasicBlock block){
         this.block = block;
+        this.solver = c;
     }
+
+    public GenericSolver<State, Context, CallEdge, iMonitoring, ?>.SolverInterface getSolverInterface(){return solver;}
 
     private State(State s){
+        registers = new ArrayList<Object>(s.registers);
         block = s.block;
         context = s.context;
-        globalstore = new HashMap<String, Set<AbstractObject>>();
-        localstore = new HashMap<String, Set<AbstractObject>>();
-        fieldstore = new HashMap<Tuple<AbstractObject, String>, Set<AbstractObject>>();
-        Set<AbstractObject> set;
-        for(Map.Entry<String, Set<AbstractObject>> i: s.globalstore.entrySet()){
-            set = new HashSet<AbstractObject>(i.getValue());
-            globalstore.put(i.getKey(), set);
+    }
+
+    /** creates a new set for the given variable in the store (used for variabledeclaraton nodes)*/
+    public void variableDeclaration(String variable, Function scope){
+        Set<AbstractObject> s = new HashSet<AbstractObject>();
+        // set will already exists when we traverse the flow graph the second time
+        if(! store.containsKey(VariableName.getVariableName(scope, variable, this)))
+            store.put(VariableName.getVariableName(scope, variable, this), s);
+    }
+
+    /** adds obj to the store set of variable in the given scope*/
+    public void writeStore(String variable, Function scope, AbstractObject obj){
+        // switch to the outer scope until the variable is defined
+        while(! store.containsKey(VariableName.getVariableName(scope, variable, this))){
+            scope = scope.getOuterFunction();
         }
-        for(Map.Entry<String, Set<AbstractObject>> i: s.localstore.entrySet()){
-            set = new HashSet<AbstractObject>(i.getValue());
-            localstore.put(i.getKey(), set);
+        //TODO: global object property if var is not found
+        // put the abstract object in the variable store
+        store.get(VariableName.getVariableName(scope, variable, this)).add(obj);
+    }
+
+    /** adds obj to the store set of variable in the given scope*/
+    public void writeStore(String variable, Function scope, Set<AbstractObject> objs){
+        // switch to the outer scope until the variable is defined
+        while(! store.containsKey(VariableName.getVariableName(scope, variable, this))){
+            scope = scope.getOuterFunction();
         }
-        for(Map.Entry<Tuple<AbstractObject, String>, Set<AbstractObject>> i: s.fieldstore.entrySet()){
-            set = new HashSet<AbstractObject>(i.getValue());
-            fieldstore.put(i.getKey(), set);
+        //TODO: global object property if var is not found
+        // put the abstract object in the variable store
+        store.get(VariableName.getVariableName(scope, variable, this)).addAll(objs);
+    }
+
+    public Set<AbstractObject> readStore(String variable, Function scope){
+        // switch to the outer scope until the variable is defined
+        while(! store.containsKey(VariableName.getVariableName(scope, variable, this))){
+            scope = scope.getOuterFunction();
+        }
+        return store.get(VariableName.getVariableName(scope, variable, this));
+    }
+
+    /** adds prop to the propertystore of obj and property */
+    public void writePropertyStore(AbstractObject obj, String property, AbstractObject prop){
+        if(! propertystore.containsKey(new Tuple<AbstractObject, String>(obj, property))){
+            Set<AbstractObject> s = new HashSet<>();
+            s.add(prop);
+            propertystore.put(new Tuple<AbstractObject, String>(obj, property), s);
+        } else {
+            propertystore.get(new Tuple<AbstractObject, String>(obj, property)).add(prop);
         }
     }
 
+    /** adds prop to the propertystore of obj and property */
+    public void writePropertyStore(AbstractObject obj, String property, Set<AbstractObject> prop){
+        if(! propertystore.containsKey(new Tuple<AbstractObject, String>(obj, property))){
+            Set<AbstractObject> s = new HashSet<>();
+            s.addAll(prop);
+            propertystore.put(new Tuple<AbstractObject, String>(obj, property), s);
+        } else {
+            propertystore.get(new Tuple<AbstractObject, String>(obj, property)).addAll(prop);
+        }
+    }
+
+    public void writeRegister(int reg,Object obj){
+        registers.set(reg, obj);
+    }
+
+    public Object readRegister(int reg){
+        return registers.get(reg);
+    }
 
     @Override
     public State clone() {
@@ -72,40 +120,32 @@ public class State implements IState <State, Context, CallEdge> {
     @Override
     public boolean propagate(State state, boolean b) {
         boolean result = false;
+        // TODO: adapt generic solver because this will always return false because of the global stores
+        /*
         Set<AbstractObject> set;
-        for(Map.Entry<String, Set<AbstractObject>> i: state.globalstore.entrySet()){
+        for(Map.Entry<String, Set<AbstractObject>> i: state.store.entrySet()){
             set = i.getValue();
-            if(globalstore.containsKey(i.getKey())){
-                result |= globalstore.get(i.getKey()).addAll(set);
+            if(store.containsKey(i.getKey())){
+                result |= store.get(i.getKey()).addAll(set);
             } else {
                 result = true;
-                globalstore.put(i.getKey(), new HashSet<AbstractObject>(i.getValue()));
+                store.put(i.getKey(), new HashSet<AbstractObject>(i.getValue()));
             }
         }
-        for(Map.Entry<String, Set<AbstractObject>> i: state.localstore.entrySet()){
+        for(Map.Entry<Tuple<AbstractObject, String>, Set<AbstractObject>> i: state.propertystore.entrySet()){
             set = i.getValue();
-            if(localstore.containsKey(i.getKey())){
-                result |= localstore.get(i.getKey()).addAll(set);
+            if(propertystore.containsKey(i.getKey())){
+                result |= propertystore.get(i.getKey()).addAll(set);
             } else {
                 result = true;
-                localstore.put(i.getKey(), new HashSet<AbstractObject>(i.getValue()));
+                propertystore.put(i.getKey(), new HashSet<AbstractObject>(i.getValue()));
             }
-        }
-        for(Map.Entry<Tuple<AbstractObject, String>, Set<AbstractObject>> i: state.fieldstore.entrySet()){
-            set = i.getValue();
-            if(fieldstore.containsKey(i.getKey())){
-                result |= fieldstore.get(i.getKey()).addAll(set);
-            } else {
-                result = true;
-                fieldstore.put(i.getKey(), new HashSet<AbstractObject>(i.getValue()));
-            }
-        }
+        }*/
         return result;
     }
 
     @Override
-    public boolean isNone() {
-        return globalstore.isEmpty() || localstore.isEmpty() || fieldstore.isEmpty();
+    public boolean isNone() {return store.isEmpty() || propertystore.isEmpty();
     }
 
     @Override
