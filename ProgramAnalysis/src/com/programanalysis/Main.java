49c639777649b7b010e21703da0a487f64ca43cd
Cedric Baumann <Cedric Baumann>
2016-05-05 09:51:27 +0200

    added new call graph construction

--- ProgramAnalysis/src/com/programanalysis/Main.java
+++ ProgramAnalysis/src/com/programanalysis/Main.java
@@ -1,16 +1,35 @@
 package com.programanalysis;
 
 import com.programanalysis.PointerAnalysis.PointerAnalysis;
+import com.programanalysis.util.CallGraphCaller;
+import com.programanalysis.util.CallGraphParser;
 import dk.brics.tajs.analysis.Analysis;
+import org.apache.commons.io.IOUtils;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.StringWriter;
+
+import static java.lang.Runtime.getRuntime;
 
 public class Main {
 
     public static void main(String[] args) {
+        // run the tajs analysis
         Analysis tajsAnalysis = dk.brics.tajs.Main.init(args, null);
         dk.brics.tajs.Main.run(tajsAnalysis);
-        PointerAnalysis analysis = new PointerAnalysis(tajsAnalysis);
+        // get the full call graph
+        //TODO: this has to be changed
+        String cg = CallGraphCaller.getCallGraph("node C:\\Users\\cedri\\Desktop\\javascript-call-graph-master\\main.js --cg  " + args[0]);
+        CallGraphParser cgp = null;
+        try {
+            cgp = new CallGraphParser(cg, args[0], tajsAnalysis);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        PointerAnalysis analysis = new PointerAnalysis(tajsAnalysis, cgp);
         analysis.init();
         analysis.solve();
         System.out.print("Pointer Analysis completed");
+
     }
 }
---
+++ ProgramAnalysis/src/com/programanalysis/util/CallGraphParser.java
@@ -0,0 +1,111 @@
+package com.programanalysis.util;
+
+import dk.brics.tajs.analysis.Analysis;
+import dk.brics.tajs.flowgraph.Function;
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+
+/**
+ * Created by cedri on 5/4/2016.
+ */
+public class CallGraphParser {
+
+    // This only works on soruce file with linux line separators -> \n
+
+    private String input;
+
+    private String sourceCode;
+
+    private Analysis analysis;
+
+    private Map<Integer, Map<Integer, Function>> functionMap;
+
+    /**
+     * constructor
+     *
+     * @param input    the text input that was given from the console execution of javascript call graph
+     * @param filePath the path to the source file that was processed by javascript call graph
+     */
+    public CallGraphParser(String input, String filePath, Analysis analysis) throws IOException {
+        this.input = input;
+        this.analysis = analysis;
+        this.functionMap = new HashMap<Integer, Map<Integer, Function>>();
+        // read in the source code
+        File file = new File(filePath);
+        BufferedReader reader = new BufferedReader(new FileReader(file));
+        String         line = null;
+        StringBuilder  stringBuilder = new StringBuilder();
+        String         ls = "\n";
+        while((line = reader.readLine()) != null) {
+            stringBuilder.append(line);
+            stringBuilder.append(ls);
+        }
+        this.sourceCode = stringBuilder.toString();
+        reader.close();
+        // build the functionMap
+        parse();
+    }
+
+    /** returns the called function by a callnode with the given source location, might return null*/
+    public Function getFunction(int srcline, int srccol){
+        Map<Integer, Function> map = functionMap.get(srcline);
+        if(map == null){
+            return null;
+        }
+        return map.get(srccol);
+    }
+
+    private void parse() throws IOException {
+        BufferedReader bufReader = new BufferedReader(new StringReader(input));
+        String line = null;
+        while ((line = bufReader.readLine()) != null) {
+            if (StringUtils.countMatches(line, "@") != 2) {
+                // we are only interested if the called function is defined in the source code
+                continue;
+            }
+            // extract the line and character numbers of the line
+            String src = line.substring(line.indexOf('@') + 1, line.indexOf(' '));
+            int srcline = Integer.parseInt(src.substring(0, src.indexOf(':')));
+            src = src.substring(src.indexOf(':') + 1);
+            int srcchar = Integer.parseInt(src.substring(0, src.indexOf('-')));
+            line = line.substring(line.indexOf('@') + 1);
+            String dest = line.substring(line.indexOf('@') + 1, line.length());
+            int destline = Integer.parseInt(dest.substring(0, dest.indexOf(':')));
+            dest = dest.substring(dest.indexOf(':') + 1);
+            int destchar = Integer.parseInt(dest.substring(0, dest.indexOf('-')));
+
+            String tmp = sourceCode.substring(0, srcchar);
+            int srccol;
+            if (tmp.lastIndexOf("\n") < 0) {
+                srccol = srcchar;
+            } else {
+                srccol = srcchar - tmp.lastIndexOf("\n");
+            }
+            tmp = sourceCode.substring(0, destchar);
+            int destcol;
+            if (tmp.lastIndexOf("\n") < 0) {
+                destcol = destchar;
+            } else {
+                destcol = destchar - tmp.lastIndexOf("\n");
+            }
+            for (Function f : analysis.getSolver().getFlowGraph().getFunctions()) {
+                if (f.getSourceLocation().getLineNumber() == destline && f.getSourceLocation().getColumnNumber() == destcol) {
+                    // we have found the right function
+                    if (functionMap.keySet().contains(srcline)) {
+                        Map<Integer, Function> map = functionMap.get(srcline);
+                        map.put(srccol, f);
+                    } else {
+                        Map<Integer,Function> map = new HashMap<Integer, Function>();
+                        map.put(srccol, f);
+                        functionMap.put(srcline, map);
+                    }
+                }
+            }
+        }
+
+    }
+}
