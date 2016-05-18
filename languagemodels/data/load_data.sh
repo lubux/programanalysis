2a4da582fc16abe6d999bdbb7457f797ba2dc651
Lukas Burkhalter <lukas.burkhalter@ringwil.ch>
2016-05-18 11:38:34 +0200

    load_data script

---
+++ languagemodels/data/load_data.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Load Files:"
+curl -L https://www.dropbox.com/sh/yx585ppchc58f83/AAD5dFNIz-B5yRjanuNr2soqa/trainingSet_part1.txt?dl=1 > trainingSet_part1.txt
+echo "File 1 done"
+curl -L https://www.dropbox.com/sh/yx585ppchc58f83/AABldiTcbWET71k1Dl2Du21Ta/trainingSet_part2.txt?dl=1 > trainingSet_part2.txt
+echo "File 2 done"
+curl -L https://www.dropbox.com/sh/yx585ppchc58f83/AADgLvGEqK-1No6_nfB7bHo2a/trainingSet_part3.txt?dl=1 > trainingSet_part3.txt
+echo "File 3 done"
