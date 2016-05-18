49aa5d8afc55c19aefa7099d90cc4f3c08ae9278
Lukas Burkhalter <lukas.burkhalter@ringwil.ch>
2016-05-18 11:20:12 +0200

    train rnn on aws script

--- languagemodels/codepred/RNNPredictor.py
+++ languagemodels/codepred/RNNPredictor.py
@@ -32,12 +32,10 @@ from __future__ import print_function
 
 import time
 import collections
-import pickle
 import os
 
 import numpy as np
 import tensorflow as tf
-from tensorflow.models.rnn import rnn
 import codepred.Vocabulary as pre
 
 
---
+++ languagemodels/train_rnn_aws.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+AWS_ADDR=ubuntu@ec2-54-93-115-43.eu-central-1.compute.amazonaws.com
+KEY_PATH=./aws/MLKey.pem
+
+SSH_CMD="sudo ssh -i $KEY_PATH $AWS_ADDR" 
+SCP_CMD="sudo scp -i $KEY_PATH "
+
+echo "create dirs on AWS"
+$SSH_CMD << EOF
+mkdir training
+cd training
+mkdir codepred
+mkdir data
+mkdir models
+cd ..
+EOF
+
+echo "upload data"
+$SCP_CMD ./codepred/RNNPredictor.py $AWS_ADDR:./training/codepred/
+$SCP_CMD ./codepred/Vocabulary.py $AWS_ADDR:./training/codepred/
+$SCP_CMD ./data/train.txt $AWS_ADDR:./training/data/
+$SCP_CMD ./data/val.txt $AWS_ADDR:./training/data/
+$SCP_CMD ./train_rnn.py $AWS_ADDR:./training/
+$SCP_CMD ./models/vocab.p $AWS_ADDR:./training/models/
+
+echo "run training"
+$SSH_CMD << EOF
+screen
+cd training
+python ./train_rnn.py | tee log.txt
+EOF
+
+echo "download data"
+$SSH_CMD << EOF
+cd training
+mv log.txt ./models/
+zip -r model_remote.zip ./models/*
+EOF
+
+$SCP_CMD $AWS_ADDR:./training/model_remote.zip .
+unzip model_remote.zip  -d ./models/
+rm model_remote.zip
+
+echo "remove data"
+$SSH_CMD << EOF
+sudo rm -r training
+EOF
