1d51d8ed08153ba27f94c5f3730a646b468eaaa4
Lukas Burkhalter <lukas.burkhalter@ringwil.ch>
2016-05-19 15:36:41 +0200

    scripts

--- suggest.sh
+++ suggest.sh
@@ -45,7 +45,7 @@ CUR_PATH=$(pwd)
 
 # Run your analysis and produce output as defined above
 cd $SCRIPT_PATH/ProgramAnalysis/
-java -jar ProgramAnalysis.jar -m test_predict -pf $1 -tf $2 > $SCRIPT_PATH/ProgramAnalysis/languagemodels/tmp_input.txt
+java -jar ProgramAnalysis.jar -m test_predict -pf $CUR_PATH/$1 -tf $CUR_PATH/$2 > $SCRIPT_PATH/languagemodels/tmp_input.txt
 cd $SCRIPT_PATH/languagemodels/
 python predict.py -m rnn -i ./tmp_input.txt
 rm tmp_input.txt
