b1ec81af4f26dc49a66eb184f39e7be2031daeff
Lukas Burkhalter <lukas.burkhalter@ringwil.ch>
2016-05-26 16:20:37 +0200

    fix 2342342343124

--- tests_nodejs/ex04.js
+++ tests_nodejs/ex04.js
@@ -4,7 +4,6 @@ function show_index(request, response) {
   var output = '<html><head><title>node.js HTTP server example</title></head><body><b>Index output</b>';
   var url_request = url.parse(request.url).query;
   output += "<p>Request query: " + url_request + "</p>";
-  if (url_request) sys.puts("Request query: ");
   response.write(output);
   response._end_();
 }
