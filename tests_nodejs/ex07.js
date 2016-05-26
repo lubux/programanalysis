11cf3f16d07698f5848d945b359366cf438193fb
Lukas Burkhalter <lukas.burkhalter@ringwil.ch>
2016-05-26 16:31:18 +0200

    fix3242342342324

--- tests_nodejs/ex07.js
+++ tests_nodejs/ex07.js
@@ -1,6 +1,10 @@
 function ender (stream) { 
-  if (stream._error) return;
-  if (!stream.isMultiPart) stream.emit("partEnd", stream);
+  if (stream._error) {
+  	return;
+  }
+  if (!stream.isMultiPart) {
+  	stream.emit("partEnd", stream);
+  }
   stream._emit_();
 }
 var stream = arg("Stream");
