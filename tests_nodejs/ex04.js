aae774d46a7582fcf836606b142dac66dcc37ecb
Lukas Burkhalter <lukas.burkhalter@ringwil.ch>
2016-05-26 16:04:30 +0200

    data fix

--- tests_nodejs/ex04.js
+++ tests_nodejs/ex04.js
@@ -1,6 +1,6 @@
 function show_index(request, response) {
   sys.puts("Serving index page");
-  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.writeHead(200);
   var output = '<html><head><title>node.js HTTP server example</title></head><body><b>Index output</b>';
   var url_request = url.parse(request.url).query;
   output += "<p>Request query: " + url_request + "</p>";
