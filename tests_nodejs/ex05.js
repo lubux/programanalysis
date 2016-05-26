18426e7ec189d1bdc6bcbb78fb013525f6bc6685
Lukas Burkhalter <lukas.burkhalter@ringwil.ch>
2016-05-26 16:26:50 +0200

    fix10

--- tests_nodejs/ex05.js
+++ tests_nodejs/ex05.js
@@ -2,7 +2,7 @@ function on_post(stream) {
   stream.setEncoding("utf8");
   stream.addListener("connect", function() {
     sys.puts("Client connected");
-    stream.write("hello\r\n");
+    stream.write("hello");
   });
   stream.addListener("data", function(data) {
     sys.puts("Received from client: " + data);
