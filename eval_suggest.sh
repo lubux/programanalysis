b45184389143c911878af875b764bea819059d56
Lukas Burkhalter <lukas.burkhalter@ringwil.ch>
2016-05-30 16:28:20 +0200

    script verbose

--- eval_suggest.sh
+++ eval_suggest.sh
@@ -9,7 +9,7 @@ CUR_PATH=$(pwd)
 
 # Run your analysis and produce output as defined above
 cd $SCRIPT_PATH/ProgramAnalysis/
-java -jar ProgramAnalysis.jar -m test_predict -pf $CUR_PATH/$1 -tf $CUR_PATH/$2 > $SCRIPT_PATH/languagemodels/tmp_input.txt 2> /dev/null
+java -jar ProgramAnalysis.jar -m test_predict -pf $CUR_PATH/$1 -tf $CUR_PATH/$2 | tee $SCRIPT_PATH/languagemodels/tmp_input.txt 2> /dev/null
 cd $SCRIPT_PATH/languagemodels/
 python predict.py -m all -i ./tmp_input.txt > sol.data
 python evaluator.py -sol $CUR_PATH/$3 -res ./sol.data
