6d3d15ee7a620739cae58c464bfd73b987e131c2
Lukas Burkhalter <lukas.burkhalter@ringwil.ch>
2016-05-31 19:31:14 +0200

    fix

--- tests_nodejs/ex30.js
+++ tests_nodejs/ex30.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var app = express();
+var app = new Object();
 var server = require('http').createServer(app);
 var Game = require('./game.js');
 var routes = require('./routes/routes.js');
