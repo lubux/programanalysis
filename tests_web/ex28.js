1adbd9427a2779dfd22ccb85ee3989e6939a0fbf
Lukas Burkhalter <lukas.burkhalter@ringwil.ch>
2016-06-06 12:05:03 +0200

    fix 28

--- tests_web/ex28.js
+++ tests_web/ex28.js
@@ -2,7 +2,7 @@ var $e = $("#seitenkopf_miniwetter");
 var codeSegments = $e.find(".weather-search__forecast-col");
 $e.find(".temperature").text(data.weatherNow + "\u00b0");
 $e.find(".weatherlabel").text(unescape(data.label));
-$e._find_(".temp .ovl_overlay").attr("class", "ovl_overlay " + symbolCodes[data.symbolCodeNow]).attr("title", unescape(data.symbolTextNow));
+$e._find_().attr("class", "ovl_overlay " + symbolCodes[data.symbolCodeNow]).attr("title", unescape(data.symbolTextNow));
 /** @type {number} */
 var i = 0;
 for (;i < codeSegments.length;i++) {
